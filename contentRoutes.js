7e6a87279111f9443a5fee63fdd3422308c59fe1
keithjohnsdev <keithjohnsdev@gmail.com>
2024-05-15 14:06:18 -0400

    adding search content by creatorId route 13

--- contentRoutes.js
+++ contentRoutes.js
@@ -896,13 +896,13 @@ router.post(
             FROM content 
             WHERE creator_user_uuid = $1 
             AND (
-                content_id = $2::integer
+                (content_id = $2::integer OR $2 ~ '^\d+$') -- Perform integer comparison if searchValue is integer
                 OR title ILIKE $3 
                 OR description ILIKE $3 
                 OR creator_name ILIKE $3 
                 OR $3 = ANY(accessibility)
                 OR $3 = ANY(tags)
-                OR $2::integer = ANY(posts) -- Explicitly cast searchValue to integer
+                OR $2 = ANY(posts) -- Perform string comparison for posts
             ) 
             ORDER BY content_id DESC
             `;
@@ -910,7 +910,7 @@ router.post(
             // Fetch content from the database for the given creatorId and filter by searchValue
             const queryResult = await db.query(query, [
                 creatorId,
-                searchValue, // For content_id and posts
+                searchValue, // For integer comparison and posts
                 `%${searchValue}%`, // For string matching
             ]);
 
