e97b3cd90cef87bf7979f9cd042303746b9baea8
keithjohnsdev <keithjohnsdev@gmail.com>
2024-05-21 21:50:28 -0400

    staging still storing on old s3, setting region with env variable

--- contentRoutes.js
+++ contentRoutes.js
@@ -13,6 +13,7 @@ const upload = multer();
 const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
 });
 
 function handleFalsyValues(req, res, next) {
